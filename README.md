8558538f780af8989190a1d6c1bc1d5c6bfb5662
Ion Bernic <bernic.ion@gmail.com>
2022-03-15 13:25:56 +0200

    0: Initial commit -> Recipe-App with services integration.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-
 Recipe app created after passing https://www.udemy.com/course/the-complete-guide-to-angular-2/
+
+To run the app:
+1. run 'npm install'
+2. run 'npm start'
+3. check 'localhost:4200'
+
+
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,35 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Rost chicken',
+      'This is a Roast Chicken',
+      'https://static.onecms.io/wp-content/uploads/sites/9/2021/02/12/roast-chicken-with-chile-basil-vinaigrette-charred-broccoli-potatoes-FT-RECIPE0321.jpg',
+      [new Ingredient("Meat", 1), new Ingredient("Brocolli", 4)]),
+    new Recipe(
+      'Salad',
+      'This is another salad',
+      'https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80',
+      [new Ingredient("Cheries", 8), new Ingredient("Lemon", 1)])
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+}
